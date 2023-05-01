9b0561628e37dd20d878756720bf060b8284b362
abdallahkshaf151 <abdallahkshaf151@gmail.com>
2023-05-01 11:55:47 +0200

    Rotating Clock

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
@@ -24,19 +26,37 @@ class MyApp extends StatelessWidget {
 }
 
 
-
 class MyWidget extends StatefulWidget {
   @override
   _MyWidgetState createState() => _MyWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  bool _isFadedIn = false;
+class _MyWidgetState extends State<MyWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
-  void _toggleFadeIn() {
-    setState(() {
-      _isFadedIn = !_isFadedIn;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _animation =
+        Tween<double>(begin: 0, end: 2 * pi).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
   }
 
   @override
@@ -44,42 +64,85 @@ class _MyWidgetState extends State<MyWidget> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor:   Colors.purple[400],
+        backgroundColor: Color(0xFFC277B8),
         title: Text(
-          'Fade-in Animation',
+          'Rotating Clock',
           style: TextStyle(fontSize: 15),
         ),
       ),
-      body: GestureDetector(
-        onTap: _toggleFadeIn,
-        child: Center(
+      body: Center(
+        child: GestureDetector(
+          onTap: _startAnimation,
           child: Container(
             width: 200,
             height: 200,
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
                 colors: [
-                  Colors.purple[400]!,
-                  Colors.pink[400]!,
-                  Colors.orange[400]!,
+                  Color(0xFF5D5FEF),
+                  Color(0xFFDF5D5F),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
               borderRadius: BorderRadius.circular(100),
-            ),
-            child: AnimatedOpacity(
-              duration: Duration(milliseconds: 500),
-              opacity: _isFadedIn ? 1 : 0,
-              child: Center(
-                child: Text(
-                  'Tap me to fade in',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  spreadRadius: 2,
                 ),
+              ],
+            ),
+            child: RotationTransition(
+              turns: _animation,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Center(
+                        child: Container(
+                          width: 6,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(4),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 5,
+                                spreadRadius: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Center(
+                        child: Container(
+                          width: 12,
+                          height: 12,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 5,
+                                spreadRadius: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
