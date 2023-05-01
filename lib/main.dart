b1486da55c78acb7d193a23a12761c6bdc7ad7ba
abdallahkshaf151 <abdallahkshaf151@gmail.com>
2023-05-01 12:17:54 +0200

    Animated Background

--- lib/main.dart
+++ lib/main.dart
@@ -16,63 +16,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'particle background animation',
+      title: 'Animated Background',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
 
-      home: ParticleBackground(
-        backgroundColor: Colors.black,
+      home: AnimatedBackground(
+
       ),
     );
   }
 }
 
-class ParticleBackground extends StatefulWidget {
-  final Color backgroundColor;
-
-  ParticleBackground({this.backgroundColor = Colors.black});
-
+class AnimatedBackground extends StatefulWidget {
   @override
-  _ParticleBackgroundState createState() => _ParticleBackgroundState();
+  _AnimatedBackgroundState createState() => _AnimatedBackgroundState();
 }
 
-class _ParticleBackgroundState extends State<ParticleBackground>
+class _AnimatedBackgroundState extends State<AnimatedBackground>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  List<Particle> particles = [];
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: Duration(seconds: 10),
       vsync: this,
-    )..addListener(() {
-      setState(() {
-        particles.removeWhere((particle) => particle.isDead);
-        for (var particle in particles) {
-          particle.update();
-        }
-      });
-    });
-
-    _controller.repeat();
-    _createParticles();
-  }
-
-  void _createParticles() {
-    for (var i = 0; i < 100; i++) {
-      particles.add(Particle());
-    }
+      duration: Duration(seconds: 1),
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+    _controller.repeat(reverse: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: widget.backgroundColor,
-      child: CustomPaint(
-        painter: ParticlePainter(particles: particles),
-      ),
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (BuildContext context, Widget? child) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.purple.withOpacity(_animation.value),
+                Colors.orange.withOpacity(_animation.value),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -81,68 +77,4 @@ class _ParticleBackgroundState extends State<ParticleBackground>
     _controller.dispose();
     super.dispose();
   }
-}
-
-class Particle {
-  double x = 0.0;
-  double y = 0.0;
-  double dx = 0.0;
-  double dy = 0.0;
-  double radius = 0.0;
-  double life = 0.0;
-  double maxLife = 0.0;
-
-  bool get isDead => life <= 0.0;
-
-  Particle() {
-    _reset();
-  }
-
-  void _reset() {
-    x = 0.0;
-    y = 0.0;
-    dx = -1.0 + Random().nextDouble() * 2.0;
-    dy = -1.0 + Random().nextDouble() * 2.0;
-    radius = 1.0 + Random().nextDouble() * 4.0;
-    maxLife = 20.0 + Random().nextDouble() * 40.0;
-    life = maxLife;
-  }
-
-  void update() {
-    x += dx;
-    y += dy;
-    life -= 0.6;
-    if (isDead) _reset();
-  }
-}
-
-class ParticlePainter extends CustomPainter {
-  final List<Particle> particles;
-  final Paint _paint = Paint()..style = PaintingStyle.fill;
-
-  ParticlePainter({required this.particles});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    particles.forEach((particle) {
-      _paint.color = HSLColor.fromAHSL(
-        1.0,
-        (particle.life / particle.maxLife) * 300.0,
-        1.0,
-        0.5,
-      ).toColor();
-
-      canvas.drawCircle(
-        Offset(
-          size.width / 2 + particle.x * 10,
-          size.height / 2 + particle.y * 10,
-        ),
-        particle.radius,
-        _paint,
-      );
-    });
-  }
-
-  @override
-  bool shouldRepaint(ParticlePainter oldDelegate) => true;
 }
