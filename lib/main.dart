7da4668ad53120148875c5f09520d76c09da814e
Abd-Allah-muhammed-muhammed <abdallahkshaf151@gmail.com>
2023-10-21 00:52:44 +0200

    black

--- lib/main.dart
+++ lib/main.dart
@@ -40,41 +40,31 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[100],
-      appBar: AppBar(
-        backgroundColor: Colors.purple[800],
-        title: Text(
-          'Toggle Container Animation',
-          style: TextStyle(fontSize: 15),
-        ),
-      ),
-      body: Column(
-        children: [
-          GestureDetector(
-            onTap: _toggleExpanded,
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              curve: Curves.easeInOut,
-              height: _isExpanded ? 600 : 200,
-              decoration: BoxDecoration(
-                color: Colors.pink[400],
+      backgroundColor: Colors.black,
 
-              ),
-              child: Center(
-                child: Text(
-                  _isExpanded ? 'Tap me to collapse' : 'Tap me to expand',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+
+      body: InkWell(
+        onTap: _toggleExpanded,
+        child: AnimatedContainer(
+
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          height: _isExpanded ? 1000 : 200,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+          ),
+          child: Center(
+            child: Text(
+              _isExpanded ? 'Tap me to collapse' : 'Tap me to expand',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-
-
-        ],
+        ),
       ),
     );
   }
